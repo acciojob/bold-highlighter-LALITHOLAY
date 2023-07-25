de267157e8e69e3ef3f3e11f843ddd51fdbc58ff
LALITHOLAY <130987029+LALITHOLAY@users.noreply.github.com>
2023-07-25 18:33:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
-    //Write your code here
-
-
+  const boldWords = document.getElementsByTagName('strong');
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = 'green';
+  }
 }
-
 
 function return_normal() {
-    //Write your code here
-
-    
+  const boldWords = document.getElementsByTagName('strong');
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = 'black';
+  }
 }
+
+const highlightLink = document.getElementById('highlightLink');
+highlightLink.addEventListener('mouseover', highlight);
+highlightLink.addEventListener('mouseout', return_normal);
+
